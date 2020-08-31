8398ea4724ff8bbdd04b80244f9245ee7e1831b7
Gabrish5411 <gabrielarrano7@yahoo.com>
2020-08-31 17:43:21 -0400

    Se agrego clase Products

---
+++ ConsoleApp/Products.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Products
+    {
+        protected string productName;
+        protected int buyPrice;
+        protected int sellPrice;
+        protected int foodConsumption;
+        protected int waterConsumption;
+        protected int minFood;
+        protected int minWater;
+        protected int foodPenalty;
+        protected int waterPenalty;
+        protected float productionTime;
+        protected float diseaseProbability;
+        protected int diseasePenalty;
+        private Random randNum = new Random(); //creado para generar probabilidad de enfermedad de manera aleatoria
+
+        public string Get_productName()
+        {
+            return productName;
+        }
+        public void Set_productName(string name)
+        {
+            productName = name;
+        }
+
+        public int Get_buyPrice()
+        {
+            return buyPrice;
+        }
+
+        public void Set_buyPrice(int value)
+        {
+            buyPrice = value;
+        }
+
+        public int Get_sellPrice()
+        {
+            return sellPrice;
+        }
+
+        public void Set_sellPrice(int value)
+        {
+            sellPrice = value;
+        }
+
+        public int Get_foodConsumption()
+        {
+            return foodConsumption;
+        }
+
+        public void Set_foodConsumption(int value)
+        {
+            foodConsumption = value;
+        }
+
+        public int Get_waterConsumption()
+        {
+            return waterConsumption;
+        }
+
+        public void Set_waterConsumption(int value)
+        {
+            waterConsumption = value;
+        }
+
+        public int Get_minFood()
+        {
+            return minFood;
+        }
+
+        public void Set_minFood(int value)
+        {
+            minFood = value;
+        }
+
+        public int Get_minWater()
+        {
+            return minWater;
+        }
+
+        public void Set_minWater(int value)
+        {
+            minWater = value;
+        }
+
+
+        public int Get_foodPenalty()
+        {
+            return foodPenalty;
+        }
+
+        public void Set_foodPenalty(int value)
+        {
+            foodPenalty = value;
+        }
+
+        public int Get_waterPenalty()
+        {
+            return waterPenalty;
+        }
+
+        public void Set_waterPenalty(int value)
+        {
+            waterPenalty = value;
+        }
+
+
+        public float Get_productionTime()
+        {
+            return productionTime;
+        }
+
+        public void Set_productionTime(float value)
+        {
+            productionTime = value;
+        }
+
+        public float Get_diseaseProbability()
+        {
+            return productionTime;
+        }
+
+        public void Set_diseaseProbability(float value)
+        {
+            productionTime = value;
+        }
+
+        public int Get_diseasePenalty()
+        {
+            return diseasePenalty;
+        }
+
+        public void Set_diseasePenalty(int value)
+        {
+            diseasePenalty = value;
+        }
+
+    }
+
+    class Seed : Products
+    {
+        private int priceVariation;
+        private float wormProbability;
+        private int wormPenalty;
+        private float undergrowthProbability;
+        private int undergowthPenalty;
+
+
+
+
+
+
+        Seed tomate = new Seed(); // tomate solo como referencia, aca faltaria crear una forma de nombrar la semilla de manera que el ususario elija
+
+
+
+
+    }
+
+    class Animal : Products
+    {
+        private int animalUnits;
+        private float escapeProbability;
+        private float suddendeathProbability;
+        private int unitsdeadRange;
+
+        Animal vaca = new Animal(); // vaca solo como referencia,  aca faltaria crear una forma de nombrar el animal de manera que el usuario lo elija 
+
+    }
+
+    class FinishedProduct //falta determinar el tipo de relacion con la clase Products
+    {
+
+
+    }
+}
