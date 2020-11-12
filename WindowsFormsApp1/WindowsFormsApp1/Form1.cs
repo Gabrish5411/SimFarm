c00795fb8141e07602e684c2d6c81ca036ba0fc4
mjanas1 <mjanas@miuandes.cl>
2020-11-12 01:13:19 -0400

    Avance en los menus de winforms

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -55,5 +55,98 @@ namespace WindowsFormsApp1
         {
             Map map = new Map(3);
         }
+
+        private void MapLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainOptions_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_AdminGranja_Click(object sender, EventArgs e)
+        {
+            MainOptions.Hide();
+            AdminGranja.Show();
+        }
+
+        private void bt_IrMercado_Click(object sender, EventArgs e)
+        {
+            MainOptions.Hide();
+            Market.Show();
+        }
+
+        private void bt_PassTurn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_GrabarPartida_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_back_AdminGranja_Click(object sender, EventArgs e)
+        {
+            AdminGranja.Hide();
+            MainOptions.Show();
+        }
+
+        private void bt_AdminProd_Click(object sender, EventArgs e)
+        {
+            AdminGranja.Hide();
+            AdminProd.Show();
+        }
+
+        private void bt_AdminAlmac_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_back_Market_Click(object sender, EventArgs e)
+        {
+            Market.Hide();
+            MainOptions.Show();
+        }
+
+        private void bt_BuildingMarket_Click(object sender, EventArgs e)
+        {
+            Market.Hide();
+            BuildingMarket.Show();
+        }
+
+        private void bt_back_BuildingMarket_Click(object sender, EventArgs e)
+        {
+            BuildingMarket.Hide();
+            Market.Show();
+        }
+
+        private void bt_back_AdminProd_Click(object sender, EventArgs e)
+        {
+            AdminProd.Hide();
+            AdminGranja.Show();
+        }
     }
 }
