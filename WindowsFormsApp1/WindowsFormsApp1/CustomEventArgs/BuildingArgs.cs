44861f43afe913da3e4e594c43f0e057b9e172b4
mjanas1 <mjanas@miuandes.cl>
2020-11-24 23:24:14 -0400

    muestra turn, wallet y funcion pasar turno

---
+++ WindowsFormsApp1/WindowsFormsApp1/CustomEventArgs/BuildingArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Buildings;
+
+namespace WindowsFormsApp1.CustomEventArgs
+{
+    public class BuildingArgs : EventArgs
+    {
+        public Building building { get; set; }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/CustomEventArgs/FieldArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Buildings;
+
+namespace WindowsFormsApp1.CustomEventArgs
+{
+    class FieldArgs : EventArgs
+    {
+        public Field field { get; set; }
+    }
+}
